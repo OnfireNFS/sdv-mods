b1374270690366924a34962183c99b2b9e7399a0
Onfyre <nathanf008@gmail.com>
2025-04-30 10:27:59 -0600

    Convert to using a store pattern

---
+++ CompanionAdventures/CompanionAdventures/Framework/Stores/Companions.cs
@@ -0,0 +1,301 @@
+using CompanionAdventures.Framework.Models;
+
+namespace CompanionAdventures.Framework;
+
+using StardewModdingAPI;
+using StardewValley;
+
+public partial class Store
+{
+    private Companions? _companions = null;
+    
+    /// <summary>
+    /// Sets the internal Companions instance if Companions is currently null. If an instance already
+    /// exists does nothing.
+    /// </summary>
+    /// <param name="companions"></param>
+    public void _Companions(Companions companions)
+    {
+        _companions ??= companions;
+    }
+    
+    public Companions UseCompanions()
+    {
+        if (_companions == null)
+            Companions.CreateStore(this);
+        
+        return _companions!;
+    }
+}
+
+public class Companions
+{
+    /****
+     ** Store Setup
+     ****/
+    private Store store;
+    private Companions(Store store)
+    {
+        this.store = store;
+    }
+    public static void CreateStore(Store store)
+    {
+        store._Companions(new Companions(store));
+    }
+    
+    /****
+     ** Companions
+     ****/
+    public Dictionary<Farmer, List<NPC>> CurrentCompanions = new();
+    
+    public int CompanionHeartsThreshold = 0;
+    public List<string> ValidCompanions = new List<string> {"Abigail", "Penny"};
+    
+    // Functions this needs to handle
+    // Add companion local/net
+    //  Remove from default scheduling
+    // Remove companion local/net
+    //  Resume default scheduling
+    // Update companion location local/net
+    // Handle game tick
+
+    public void DrawCompanions(Farmer farmer)
+    {
+        foreach (var entry in CurrentCompanions)
+        {
+            // If the selected row is not our current farmer skip it
+            if (entry.Key != farmer)
+            {
+                continue;
+            }
+
+            foreach (NPC npc in entry.Value)
+            {
+                npc.position.X = (int)farmer.position.X;
+                npc.position.Y = (int)farmer.position.Y;
+            }
+        }
+        
+        // For NPC in CompanionManager.CurrentCompanions
+        // check if NPC farmer is current farmer
+        // update NPC location to follow current farmer
+    }
+    
+    /// <summary>
+    /// Attempts to add the npc to the farmers companion list
+    /// </summary>
+    /// <returns>
+    /// Returns true if the companion was added or false if it couldn't be added.
+    /// </returns>
+    public bool AddCompanion(Farmer farmer, NPC npc)
+    {
+        ModConfig config = store.UseConfig();
+        IMonitor monitor = store.UseMonitor();
+        
+        monitor.Log($"Attempting to add {npc.Name} as a companion to {farmer.Name}.", LogLevel.Trace);
+        
+        // Check if NPC is already a companion
+        if (IsCompanion(npc))
+        {
+            monitor.Log($"Could not add {npc.Name} as a companion to {farmer.Name}. {npc.Name} is already a companion!", LogLevel.Trace);
+            return false;
+        }
+        
+        // Get farmers list of companions if they have one
+        if (CurrentCompanions.TryGetValue(farmer, out List<NPC>? companions))
+        {
+            // If farmer has more than or equal to the maximum number of companions
+            if (companions.Count >= config.MaxCompanions)
+            {
+                monitor.Log(
+                    $"Could not add {npc.Name} as a companion to {farmer.Name}. {farmer.Name} already has the maximum number of companions!",
+                    LogLevel.Trace
+                );
+                return false;
+            }
+
+            // If farmer doesn't have the ore than or equal to the maximum number of companions, add this companion
+            companions.Add(npc);
+            monitor.Log($"Successfully added {npc.Name} as a companion to {farmer.Name}.", LogLevel.Trace);
+            CompanionData data = new CompanionData(farmer, npc);
+            
+            IModHelper helper = store.UseHelper();
+            IManifest modManifest = store.UseManifest();
+            
+            helper.Multiplayer.SendMessage(data, Constants.MessagetypeCompanionAdded, new []{ modManifest.UniqueID });
+            return true;
+        }
+        // Farmer doesn't exist in dictionary, add them and create a new list
+        // Because farmer doesn't exist this means that they must have 0 companions, and it should be safe to add this
+        // companion
+        else
+        {
+            CurrentCompanions.Add(farmer, new List<NPC> { npc });
+            monitor.Log($"Successfully added {npc.Name} as a companion to {farmer.Name}.", LogLevel.Trace);
+            return true;
+        }
+    }
+
+    public bool IsNpcValidCompanion(NPC npc)
+    {
+        IMonitor monitor = store.UseMonitor();
+        
+        monitor.Log($"Checking if {npc.Name} can be a valid companion.", LogLevel.Trace);
+
+        if (ValidCompanions.Contains(npc.Name))
+        {
+            monitor.Log($"{npc.Name} can be a companion.", LogLevel.Trace);
+            return true;
+        }
+        
+        monitor.Log($"{npc.Name} can not be a companion.", LogLevel.Trace);
+        return false;
+    }
+
+    public bool IsNpcValidCompanionForFarmer(Farmer farmer, NPC npc)
+    {
+        IMonitor monitor = store.UseMonitor();
+        
+        monitor.Log($"Checking if {npc.Name} can be a valid companion for {farmer.Name}.", LogLevel.Trace);
+        // Early Exit: If NPC can't be a companion return
+        if (!IsNpcValidCompanion(npc))
+            return false;
+        
+        // Get the heart level of the farmer and this npc
+        var hearts = Util.GetHeartLevel(farmer, npc);
+
+        // Return true if number of hearts is equal to or above heart threshold
+        if (hearts >= CompanionHeartsThreshold)
+        {
+            monitor.Log($"{npc.Name} can be a valid companion for {farmer.Name}.", LogLevel.Trace);
+            return true;
+        }
+        
+        monitor.Log($"{npc.Name} is not a valid companion for {farmer.Name}.", LogLevel.Trace);
+        return false;
+    }
+    
+    public List<NPC>? GetCurrentCompanions(Farmer player)
+    {
+        return CurrentCompanions.GetValueOrDefault(player);
+    }
+
+    public bool IsCompanion(NPC npc)
+    {
+        foreach (var entry in CurrentCompanions)
+        {
+            if (entry.Value.Contains(npc))
+                return true;
+        }
+
+        return false;
+    }
+
+    public bool IsCurrentlyCompanionForFarmer(Farmer farmer, NPC npc)
+    {
+        // Try to get companions for this farmer
+        if (CurrentCompanions.TryGetValue(farmer, out List<NPC>? companions))
+        {
+            // Check if list of companions contains NPC
+            return companions.Contains(npc);
+        }
+
+        // Farmer does not have any companions so this npc isn't currently a companion
+        return false;
+    }
+
+    public void CompanionAdd(Farmer farmer, NPC npc)
+    {
+        IMonitor monitor = store.UseMonitor();
+        
+        // Create dialog options
+        string dialogText = $"Ask {npc.Name} to follow?";
+        Response[] responses =
+        [
+            new Response("yes_key", "Yes"),
+            new Response("no_key", "No"),
+        ];
+                
+        void AfterQuestionBehaviour(Farmer farmer, string responseText)
+        {
+            if (responseText == "yes_key")
+            {
+                AddCompanion(farmer, npc);
+                monitor.Log($"Is {npc.Name} a companion? {IsCompanion(npc)}");
+            }
+            else
+            {
+                        
+            }
+        }
+
+        Game1.currentLocation.createQuestionDialogue(dialogText, responses, AfterQuestionBehaviour, npc);
+    }
+
+    public void CompanionOptions(Farmer farmer, NPC npc)
+    {
+        IMonitor monitor = store.UseMonitor();
+        
+        string dialogText = $"Ask {npc.Name} to leave?";
+        Response[] responses =
+        [
+            new Response("yes_key", "Yes"),
+            new Response("no_key", "No"),
+        ];
+                    
+        void AfterQuestionBehaviour(Farmer farmer, string responseText)
+        {
+            if (responseText == "yes_key")
+            {
+                monitor.Log($"Is {npc.Name} a companion? {IsCompanion(npc)}");
+            }
+            else
+            {
+                        
+            }
+        }
+
+        Game1.currentLocation.createQuestionDialogue(dialogText, responses, AfterQuestionBehaviour, npc);
+    }
+
+    public void CompanionUpdate()
+    {
+        
+    }
+
+    /****
+     ** Events
+     ****/
+    public void OnCompanionAdded(CompanionData data)
+    {
+        
+    }
+
+    public void OnCompanionRemoved(CompanionData data)
+    {
+        
+    }
+
+    /// <summary>
+    /// Uses the provided data to update the provided companion with the provided information
+    ///
+    /// Useful when the companion is managed by another source (eg: another player in a multiplayer game) and the
+    /// position or state information has been already calculated by another client and the display just needs to be
+    /// updated in this client.
+    /// </summary>
+    /// <param name="data"></param>
+    public void OnCompanionUpdated(CompanionData data)
+    {
+        
+    }
+
+    public void OnPlayerWarped(Farmer farmer)
+    {
+        // Update current farmers NPCs so that they warp to the same map as farmer
+    }
+
+    public void OnUpdateTicking()
+    {
+        // Draw all NPCs
+    }
+}
---
+++ CompanionAdventures/CompanionAdventures/Framework/Stores/Events.cs
@@ -0,0 +1,168 @@
+using CompanionAdventures.Framework.Models;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace CompanionAdventures.Framework;
+
+public partial class Store
+{
+    private Events? _events = null;
+
+    public void _Events(Events events)
+    {
+        _events ??= events;
+    }
+
+    public Events UseEvents()
+    {
+        if (_events == null)
+            Events.CreateStore(this);
+
+        return _events!;
+    }
+}
+
+public class Events
+{
+    /****
+     ** Store Setup
+     ****/
+    private Store store;
+    private Events(Store store)
+    {
+        this.store = store;
+    }
+    public static void CreateStore(Store store)
+    {
+        store._Events(new Events(store));
+    }
+    
+    /****
+     ** Events
+     ****/
+
+    public void RegisterEvents()
+    {
+        IModHelper helper = store.UseHelper();
+        IModEvents events = helper.Events;
+        
+        events.GameLoop.GameLaunched += OnGameLaunched;
+        events.GameLoop.UpdateTicking += OnUpdateTicking;
+        events.Input.ButtonPressed += OnButtonPressed;
+        events.Multiplayer.ModMessageReceived += OnMessageReceived;
+        events.Player.Warped += OnPlayerWarped;
+    }
+    
+    private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        IModHelper helper = store.UseHelper();
+        
+        var api = helper.ModRegistry.GetApi<IContentPack>("Pathoschild.ContentPatcher");
+    }
+    
+    private void OnUpdateTicking(object? sender, UpdateTickingEventArgs e)
+    {
+        Companions companions = store.UseCompanions();
+        
+        companions.OnUpdateTicking();
+    }
+    
+    
+    /// <summary>
+    /// Handles "OnButtonPressed" events for the CompanionAdventures mod.
+    ///
+    /// More specifically will check if the button pressed was the action button, if the farmer is trying to
+    /// interact with a NPC and if the NPC has interactions relating to CompanionAdventures.
+    /// </summary>
+    /// <param name="sender">The event sender.</param>
+    /// <param name="e">The event data.</param>
+    private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+    {
+        // Ignore if player isn't in the world or if they are in a cutscene
+        if (!Context.IsPlayerFree)
+            return;
+
+        // Check if the button pressed is the interact button
+        if (e.Button.IsActionButton())
+        {
+            // Get the first NPC in the tile that the cursor is currently over
+            NPC? npc = Util.GetFirstNpcFromCursor(e.Cursor);
+
+            // Early Exit: If there is no NPC there is nothing to do
+            if (npc == null)
+                return;
+            
+            // Update the currentDialogue in case it is outdated
+            var farmer = Game1.player;
+            var heartLevel = Util.GetHeartLevel(farmer, npc);
+            npc.checkForNewCurrentDialogue(heartLevel);
+            
+            Companions companions = store.UseCompanions();
+            
+            // If NPC is currently not a companion and has a Dialogue message queued --or--
+            // NPC cannot be a companion for this farmer:
+            // Do nothing and let the dialogue message trigger or default behaviour trigger
+            if (npc.CurrentDialogue.Count > 0 || !companions.IsNpcValidCompanionForFarmer(farmer, npc))
+            {
+                return;
+            }
+            
+            // Suppress default behavior 
+            IModHelper helper = store.UseHelper();
+            helper.Input.Suppress(e.Button);
+            
+            // Is the NPC being talked to currently a companion?
+            if (companions.IsCurrentlyCompanionForFarmer(farmer, npc))
+            {
+                // If yes, show options for this companion
+                companions.CompanionOptions(farmer, npc);
+            }
+            else
+            {
+                // If no, show the option to add this NPC as a companion
+                companions.CompanionAdd(farmer, npc);
+            }
+        }
+    }
+    
+    private void OnMessageReceived(object? sender, ModMessageReceivedEventArgs e)
+    {
+        IManifest modManifest = store.UseManifest();
+        
+        // Early Exit: The message received was from a different mod
+        if (e.FromModID != modManifest.UniqueID)
+        {
+            return;
+        }
+        
+        Companions companions = store.UseCompanions();
+        IMonitor monitor = store.UseMonitor();
+
+        switch (e.Type)
+        {
+            case Constants.MessagetypeCompanionAdded:
+                companions.OnCompanionAdded(e.ReadAs<CompanionData>());
+                break;
+            case Constants.MessagetypeCompanionRemoved:
+                companions.OnCompanionRemoved(e.ReadAs<CompanionData>());
+                break;
+            case Constants.MessagetypeCompanionUpdated:
+                companions.OnCompanionUpdated(e.ReadAs<CompanionData>());
+                break;
+            default:
+            {
+                string data = e.ReadAs<string>();
+                monitor.Log($"Received unknown event type: \"{e.Type}\" event with data: \"{data}\"", LogLevel.Error);
+                break;
+            }
+        }
+    }
+
+    private void OnPlayerWarped(object? sender, WarpedEventArgs e)
+    {
+        Companions companions = store.UseCompanions();
+        
+        companions.OnPlayerWarped(e.Player);
+    }
+}
